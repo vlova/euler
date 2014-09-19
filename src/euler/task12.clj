7957a0d6a7c403400873bc68506f6a4763874468
Viktor Lova <nsinreal@gmail.com>
2014-09-19 16:44:20 +0300

    +

--- src/euler/task12.clj
+++ src/euler/task12.clj
@@ -1,45 +1,13 @@
 (ns euler.task12)
 
 (use 'clojure.math.numeric-tower)
+(use 'euler.primes)
 
 (defn- triangle-number [i]
   {:doc "return i-th triangle number"}
 
   {:number (/ (* i (inc i)) 2)
    :index i})
-
-(defn- divides? [number div]
-  (= (mod number div) 0))
-
-(defn- prime-divisors-count [number primes]
-  {:doc "count number of prime divisors of given number" }
-
-  (let [limit (inc (bigint (sqrt number)))]
-    (count
-     (->> primes
-          (filter #(< % limit))
-          (map #(divides? number %))
-          (filter true?)))))
-
-(defn- isPrime? [number primes]
-  {:doc "checks if given number is prime using precalculated array of primes"}
-
-  (zero? (prime-divisors-count number primes)))
-
-(defn- primes
-  {:doc "generates array of prime numbers" }
-
-  ([up-to]
-   (loop [previous 3
-          generated [2 3]]
-     (if (>= previous up-to)
-       generated
-       (let [current (+ previous 2)]
-         (if (isPrime? current generated)
-           (recur current (conj generated current))
-           (recur current generated))))
-     )
-   ))
 
 (defn- divide-times [number divisor]
   {:doc "determine how many times number can be divided using given divisor"}
@@ -110,4 +78,4 @@
           (recur (inc i)))
         ))))
 
-(time (task 500))
+;(time (task 500))
