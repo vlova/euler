5fd2b70f43d13d6bc96a95d69ee4afd590baa268
Viktor Lova <nsinreal@gmail.com>
2014-10-13 20:54:15 +0300

    problem 14: updated

--- src/euler/task14.clj
+++ src/euler/task14.clj
@@ -5,21 +5,29 @@
 
 (def collatz-cache (java.util.HashMap.))
 
-(defn collatz [n]
+(defn- collatz [n]
   (with-cache collatz-cache n
     (fn []
       (if (= n 1)
-        [{:number 1 :count 1}]
+        [{:number 1
+          :count 1}]
+
         (let [rest (if (even? n)
                      (collatz (/ n 2))
                      (collatz (inc (* 3 n))))
               rest-count (:count (first rest))]
-          (cons {:number n :count (inc rest-count)}
+          (cons {:number n
+                 :count (inc rest-count)}
                 rest))))))
 
+(defn find-max [f coll]
+  (reduce (fn [a b] (if (>= (f a) (f b)) a b))
+          {:count 1 :number 1}
+          coll))
+
 (defn task []
-  (first (sort-by (comp - :count)
+  (find-max :count
            (map (comp first collatz)
-                (range 1 1000001)))))
+                (range 1 1000001))))
 
 (time (task))
